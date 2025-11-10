a023f9d93235254f5301df4348fc2bbaae823d3f
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-11-10 09:54:12 -0330

    Item abstract class

---
+++ Assignment3/Item.java
@@ -0,0 +1,11 @@
+package Assignment3;
+
+public abstract class Item {
+    String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public abstract Action getAction();
+}
