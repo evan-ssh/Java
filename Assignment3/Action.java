f25b98cce0b921f97412eaaf3c878546bebcb5fd
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-11-10 09:52:56 -0330

    Action and actor classes

---
+++ Assignment3/Action.java
@@ -0,0 +1,5 @@
+package Assignment3;
+
+public interface Action {
+    void perform(Actor src, Actor trg);
+}
---
+++ Assignment3/Actor.java
@@ -0,0 +1,45 @@
+package Assignment3;
+
+import java.awt.Desktop;
+
+public class Actor {
+    String name;
+    int health;
+    int defense;
+    int stamina;
+    int skillPoints;
+    Actor target;
+    Item[] inventory;
+
+    public Actor(String name, int health, int defense, int stamina, int skillPoints) {
+        this.name = name;
+        this.health = health;
+        this.defense = defense;
+        this.stamina = stamina;
+        this.skillPoints = skillPoints;
+        this.inventory = new Item[5];
+    }
+
+    public void selectTarget(Actor target) {
+        this.target = target;
+    }
+
+    public void performAction(Action action) {
+        if(target == null){
+            System.out.println("No target selected.");
+            return;
+        }
+        action.perform(this, target);
+    }
+
+    public Action useItem(int index) {
+        if(index < 0 || index >= inventory.length || inventory[index] == null){
+            System.out.println("Invalid item selection.");
+            return null;
+        }
+        Item item = inventory[index];
+        return item.getAction();
+    }
+
+
+}
