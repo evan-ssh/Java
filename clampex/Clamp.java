00a4246d4ab3b7ed3b34f906a14dd485e0eb6ea7
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-12-15 01:46:59 -0330

    Clamp warmup return min or max

---
+++ clampex/Clamp.java
@@ -0,0 +1,13 @@
+package clampex;
+
+public class Clamp {
+    static int clamp(int value, int min, int max){
+        if(value < min){
+            return min;
+        }else if (value > max) {
+            return max;
+        }else{
+            return value;
+        }
+    }
+}
